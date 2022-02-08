cdca3bb62cc6062c8fb353d3ffcb2d56231cdfb4
Roman <roman.d.kozlov@gmail.com>
2022-02-08 18:41:20 +0300

    code is done. interface will be moderniz

--- SliderColorChange/ViewController.swift
+++ SliderColorChange/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setViewColor()
+        setValueLabels()
     }
-
-
+    
+    @IBAction func redSliderChange() {
+        setValueLabels()
+        setViewColor()
+    }
+    
+    @IBAction func greenSliderChange() {
+        setValueLabels()
+        setViewColor()
+    }
+    
+    @IBAction func blueSliderChange() {
+        setValueLabels()
+        setViewColor()
+    }
+    
+    private func setViewColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func setValueLabels() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
